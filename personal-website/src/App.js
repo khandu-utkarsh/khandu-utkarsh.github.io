40a350e04786843b80d6d25e0d3dc5f4b16c52ce
utkarsh <utkarshkhandelwal2011@gmail.com>
2023-10-12 21:13:50 -0400

    Made a change

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Utkarsh Khandelwal
+        </h1>
+        <p>Coming Soon... </p>
         <a
           className="App-link"
           href="https://reactjs.org"
