4694d68bbe6c750d863c5d384e796e32f77c19dd
utkarsh <utkarshkhandelwal2011@gmail.com>
2023-10-22 00:15:04 -0400

    Showing in development for undeveloped pages.

--- personal-website/src/components/blogs/Blogs.js
+++ personal-website/src/components/blogs/Blogs.js
@@ -5,7 +5,8 @@ function Blogs() {
         return (
             <Container maxWidth="lg" component={"span"}>
             <Typography component={"span"}>
-            Under Construction: I am facing a significant dilemma—should I first create a dynamic blogging framework and integrate it here, or should I start by using cards to display static content? Whatever it is, I will soon work on it.
+            {/* Under Construction: I am facing a significant dilemma—should I first create a dynamic blogging framework and integrate it here, or should I start by using cards to display static content? Whatever it is, I will soon work on it. */}
+            Under development.
             </Typography>
         </Container>
         );
--- personal-website/src/components/resume/Resume.js
+++ personal-website/src/components/resume/Resume.js
@@ -4,8 +4,9 @@ function Resume() {
         return (
             <Container maxWidth="lg" component={"span"}>
             <Typography component={"span"}>
-            Under Construction: I will work on this last. I haven't thought about the design yet. I am not looking for anything fancy, like having lots of icons, etc., and making it cluttered. 
-            Instead, I'm looking for something plain and simple. If I can use typesetting or markdown, it would be extremely helpful.
+            {/* Under Construction: I will work on this last. I haven't thought about the design yet. I am not looking for anything fancy, like having lots of icons, etc., and making it cluttered. 
+            Instead, I'm looking for something plain and simple. If I can use typesetting or markdown, it would be extremely helpful. */}
+            Under development.
             </Typography>
         </Container>
         );
