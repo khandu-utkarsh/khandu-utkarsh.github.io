9d2d6dcc1f50f08154ec5a0299c05213462f8244
utkarsh <utkarshkhandelwal2011@gmail.com>
2023-10-23 19:10:51 -0400

    Added under development text

--- personal-website/src/components/blogs/Blogs.js
+++ personal-website/src/components/blogs/Blogs.js
@@ -5,7 +5,6 @@ function Blogs() {
         return (
             <Container maxWidth="lg" component={"span"}>
             <Typography component={"span"}>
-            {/* Under Construction: I am facing a significant dilemma—should I first create a dynamic blogging framework and integrate it here, or should I start by using cards to display static content? Whatever it is, I will soon work on it. */}
             Under development.
             </Typography>
         </Container>
