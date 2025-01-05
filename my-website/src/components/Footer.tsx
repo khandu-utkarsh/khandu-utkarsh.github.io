04ae50f2ded5269ef5cf4b782011ce70721c13c6
utkarsh <utkarshkhandelwal2011@gmail.com>
2025-01-05 17:13:08 -0500

    Fixed few issues in footer.

--- my-website/src/components/Footer.tsx
+++ my-website/src/components/Footer.tsx
@@ -1,56 +1,24 @@
 "use client"
 
 import * as React from 'react';
-import Link from 'next/link'
-import { Paper, Tab, Tabs, Grid2 as Grid } from '@mui/material';
+import { Paper, Box, Grid2 as Grid, Typography } from '@mui/material';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 
-interface LinkTabProps {
-  label?: string;
-  href?: string;
-  selected?: boolean;
-}
-
-interface NameTabProps {
-    label?: string;
-    selected?: boolean;
-}
-
-function LinkTab(props: LinkTabProps) {
-
-    const { label, href, selected, ...rest } = props;
-    return (
-        <Link href={href || "#"} passHref>
-            <Tab label={label} {...rest} />
-        </Link>
-    );
-}
-
-function NameTab(props: NameTabProps) {
-    const {label, selected, ...rest } = props;
-    return (
-        <Tab label={label} component="div" {...rest}></Tab>
-    )
-}
+//!Icons
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 export default function Footer() {
 
 return (
-    <Paper>
-        <Tabs value={false} role="navigation">
-            <Grid container direction="row" sx={{ flexGrow: 1}}>
-                <Grid size={4}>
-                <NameTab label="Utkarsh Khandelwal"></NameTab>
-                </Grid>
-                <Grid size={8} container sx={{justifyContent: "flex-end"}}>
-                    <LinkTab label="Home" href="/" />
-                    <LinkTab label="Projects" href="/projects" />
-                    <LinkTab label="Resume" href="/resume" />
-                    <LinkTab label="Writings" href="/writings" />
-                    <LinkTab label="Photos" href="/photos" />                    
-                </Grid>
-            </Grid>            
-        </Tabs>
-    </Paper>
- );
+
+    <Grid container direction="row" sx={{ flexGrow: 1, justifyContent: "center", alignItems: "center"}}>
+        <Typography variant= "body1">  Made with <FavoriteIcon color='inherit' fontSize='inherit' sx={{color: "red"}}/> in Cambridge.</Typography>
+    </Grid>
+
+);
 }
